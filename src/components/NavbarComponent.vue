e4fa7055242451737a78d626ef76271a2ec8b44f
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-29 02:10:00 -0300

    css: adding the name of the site catalog. styling based on the chosen color palette

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-  $END$
+  <nav class="flex bg-[#322886] h-[70px]">
+    <h3 class="flex text-[#ebebeb] font-bold text-3xl items-center">MII Catalog</h3>
+  </nav>
 </template>
 
 <style scoped>
