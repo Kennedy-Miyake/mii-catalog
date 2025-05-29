6d21bf172a766572ffa07ea19728ebe265a84c82
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-29 08:59:59 -0300

    mod: added the card to the product cards section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,6 +21,10 @@ onMounted(async() => {
 
 <template>
   <section class="relative grid grid-cols-7 grid-rows-3 w-[1575px] h-[850px] ml-auto mt-[75px] mr-[15px] p-6 rounded-2xl bg-black">
+    <ProductCard
+      v-for="p in products"
+      :product="p"
+    />
     <div class="absolute bottom-1 right-6 flex gap-16 mr-2">
       <button class="px-4 py-2 rounded-lg bg-[#322886] text-white cursor-pointer">Prev</button>
       <button class="px-4 py-2 rounded-lg bg-[#322886] text-white cursor-pointer">Next</button>
