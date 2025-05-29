a3f97bc7ccadf638078b0b72ba69f4d7ea60613f
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-29 02:57:09 -0300

    feat: added a search button. adjusted the search bar. stylized search bar

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -9,8 +9,14 @@
       <input
           type="text"
           placeholder="Busque seus produtos..."
-          class="w-full"
+          class="w-[500px] h-[40px] border-2 border-r-0 border-[#ededed] focus:outline-none focus:ring-0 rounded-l-md text-[#ededed] px-4"
       />
+      <button
+        type="submit"
+        class="h-[40px] mr-[30px] border-2 border-l-0 border-[#ededed] rounded-r-md px-2 cursor-pointer"
+      >
+        <img src="../assets/icons/search-svgrepo-com.svg" alt="search" class="h-[30px]">
+      </button>
     </form>
   </nav>
 </template>
