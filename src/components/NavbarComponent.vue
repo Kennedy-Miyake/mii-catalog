4a32674e2b7a4289b2e3f535e7b5ee12c756bd74
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-29 02:17:59 -0300

    feat: added a search bar

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -5,6 +5,13 @@
 <template>
   <nav class="flex bg-[#322886] h-[70px]">
     <h3 class="flex text-[#ebebeb] font-bold text-3xl items-center">MII Catalog</h3>
+    <form class="flex items-center ml-auto">
+      <input
+          type="text"
+          placeholder="Busque seus produtos..."
+          class="w-full"
+      />
+    </form>
   </nav>
 </template>
 
