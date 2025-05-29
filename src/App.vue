83abdf1b2863afb3ba89aa3e95e3b05782d24898
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-29 01:42:34 -0300

    catalog project created with vite, vue.js and tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <main class="flex flex-col min-h-screen bg-[#ededed]">
+    <router-view></router-view>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
