ef8f570d53a66475c18c1123119924d64d6e3551
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-29 04:05:42 -0300

    css: stylized category items

--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -3,25 +3,24 @@
 </script>
 
 <template>
-<div class="fixed top-[75px] left-[30px] flex flex-col bg-green-400 w-[300px] h-[850px] z-50">
-  <h3 class="flex mx-auto text-[#ebebeb] font-bold text-3xl bg-red-300">Categorias</h3>
-  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
-  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
-  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
-  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
-  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
-  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
-  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
-  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
-  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
-  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
-  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
-  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
-  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
-  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
-  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
-  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
-  <div class="flex items-center w-full h-[30px] px-6 mb-2 mt-auto bg-blue-400">
+<div class="fixed top-[75px] left-[15px] flex flex-col bg-[#322886] w-[300px] h-[850px] rounded-2xl z-50">
+  <h3 class="flex mx-auto text-[#ebebeb] font-bold text-3xl mt-4">Categorias</h3>
+  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
+  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
+  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
+  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
+  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
+  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
+  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
+  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
+  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
+  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
+  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
+  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
+  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
+  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
+  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
+  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
     <button class="text-red-500 w-[70px] h-full bg-black rounded-md">prev</button>
     <button class="text-red-500 w-[70px] h-full bg-black rounded-md ml-auto">next</button>
   </div>
