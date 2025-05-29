e20938a9e06901e91f7c902831de67bc9c1acc75
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-29 05:43:07 -0300

    feat: created a card that will be the basis for the product card component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,14 @@
 </script>
 
 <template>
-  <section class="w-[1575px] h-[850px] ml-auto mt-[75px] mr-[15px] rounded-2xl bg-black"></section>
+  <section class="grid grid-cols-7 grid-rows-3 w-[1575px] h-[850px] ml-auto mt-[75px] mr-[15px] p-6 rounded-2xl bg-black">
+    <div class="flex flex-col items-center w-[200px] h-[230px] bg-gray-500 mx-3 my-3 rounded-lg">
+      <div class="w-[80px] h-[80px] bg-white mt-4"></div>
+      <p class="flex w-full h-[35px] text-mb text-wrap text-center font-semibold justify-center items-center bg-red-300 mt-2">Nome do Produto</p>
+      <p class="flex w-full h-[35px] text-mb text-wrap text-center font-semibold items-center bg-red-300 mt-2 pl-4">R$99.90</p>
+      <p class="flex w-full h-[35px] text-mb text-wrap text-center font-semibold justify-center items-center bg-red-300 mt-2">Estoque</p>
+    </div>
+  </section>
 </template>
 
 <style scoped>
