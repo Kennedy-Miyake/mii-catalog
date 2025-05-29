f9aa345e3df5cc5862bcf152e203b4183e7cf623
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-29 03:51:50 -0300

    mod: added test categories to the categories sidebar

--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -3,7 +3,24 @@
 </script>
 
 <template>
-<div class="fixed top-[75px] left-[30px] flex flex-col bg-[#000000] w-[300px] h-[850px] z-50">
+<div class="fixed top-[75px] left-[30px] flex flex-col bg-green-400 w-[300px] h-[850px] z-50">
+  <h3 class="flex mx-auto text-[#ebebeb] font-bold text-3xl bg-red-300">Categorias</h3>
+  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
+  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
+  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
+  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
+  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
+  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
+  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
+  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
+  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
+  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
+  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
+  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
+  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
+  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
+  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
+  <div class="flex justify-center items-center w-full h-[40px] bg-gray-500 mt-2">aaaa</div>
 </div>
 </template>
 
