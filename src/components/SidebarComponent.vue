d903a4a4a131cba779e227d13cb24903dcc5e0fb
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-30 02:21:33 -0300

    mod: categories added to the sidebar

--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -24,6 +24,11 @@ onMounted(async () => {
 <template>
 <div class="fixed grid grid-rows-16 top-[75px] left-[15px] flex flex-col bg-[#322886] w-[300px] h-[850px] rounded-2xl z-50">
   <h3 class="flex mx-auto text-[#ebebeb] font-bold text-3xl mt-4">Categorias</h3>
+  <div
+      v-for="category in categoriesSide"
+      class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">
+    {{ category }}
+  </div>
   <div class="flex absolute bottom-1 items-center w-full h-[30px] px-6 mb-2 mt-auto">
     <button class="text-[#322886] w-[70px] h-full bg-[#ebebeb] border hover:text-[#ebebeb] hover:bg-transparent hover:border-[#ebebeb] cursor-pointer rounded-md">prev</button>
     <button class="text-[#322886] w-[70px] h-full bg-[#ebebeb] border hover:text-[#ebebeb] hover:bg-transparent hover:border-[#ebebeb] cursor-pointer rounded-md ml-auto">next</button>
