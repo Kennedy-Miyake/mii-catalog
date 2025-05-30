28422b3a8bc04ff9d0ad9b24dad47dac238c0307
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-29 21:15:41 -0300

    mod: adjusting the buttons by leaving them fixed

--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -3,25 +3,10 @@
 </script>
 
 <template>
-<div class="fixed top-[75px] left-[15px] flex flex-col bg-[#322886] w-[300px] h-[850px] rounded-2xl z-50">
+<div class="fixed grid grid-rows-16 top-[75px] left-[15px] flex flex-col bg-[#322886] w-[300px] h-[850px] rounded-2xl z-50">
   <h3 class="flex mx-auto text-[#ebebeb] font-bold text-3xl mt-4">Categorias</h3>
   <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
-  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
-  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
-  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
-  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
-  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
-  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
-  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
-  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
-  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
-  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
-  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
-  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
-  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
-  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
-  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
-  <div class="flex items-center w-full h-[30px] px-6 mb-2 mt-auto">
+  <div class="flex absolute bottom-1 items-center w-full h-[30px] px-6 mb-2 mt-auto">
     <button class="text-[#322886] w-[70px] h-full bg-[#ebebeb] border hover:text-[#ebebeb] hover:bg-transparent hover:border-[#ebebeb] cursor-pointer rounded-md">prev</button>
     <button class="text-[#322886] w-[70px] h-full bg-[#ebebeb] border hover:text-[#ebebeb] hover:bg-transparent hover:border-[#ebebeb] cursor-pointer rounded-md ml-auto">next</button>
   </div>
