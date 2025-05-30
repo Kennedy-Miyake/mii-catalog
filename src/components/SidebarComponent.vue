7580cda9c22e75b86edcab5ce3f5606c434fc542
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-30 02:20:32 -0300

    feat: created the function to get all the categories and set a limit to print in the sidebar

--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { getCategories } from '../assets/services/products.js'
 
+const categoriesTotal = ref([])
+const categoriesSide = ref([])
+const limit = 14
+const skip = ref(0)
+
+const fetchCategories = async () => {
+  const { data } = await getCategories()
+  categoriesTotal.value = data
+  console.log(categoriesTotal.value)
+
+  categoriesSide.value = categoriesTotal.value.slice(skip.value, skip.value+limit)
+  console.log(categoriesSide.value)
+}
+
+onMounted(async () => {
+  fetchCategories()
+})
 </script>
 
 <template>
 <div class="fixed grid grid-rows-16 top-[75px] left-[15px] flex flex-col bg-[#322886] w-[300px] h-[850px] rounded-2xl z-50">
   <h3 class="flex mx-auto text-[#ebebeb] font-bold text-3xl mt-4">Categorias</h3>
-  <div class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">Hardware</div>
   <div class="flex absolute bottom-1 items-center w-full h-[30px] px-6 mb-2 mt-auto">
     <button class="text-[#322886] w-[70px] h-full bg-[#ebebeb] border hover:text-[#ebebeb] hover:bg-transparent hover:border-[#ebebeb] cursor-pointer rounded-md">prev</button>
     <button class="text-[#322886] w-[70px] h-full bg-[#ebebeb] border hover:text-[#ebebeb] hover:bg-transparent hover:border-[#ebebeb] cursor-pointer rounded-md ml-auto">next</button>
