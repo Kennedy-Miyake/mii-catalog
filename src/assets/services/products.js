86b5d53d22dbe3a24456d1fe488b6b5cd50e6f83
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-30 07:29:15 -0300

    feat: added endpoint to search for products in your category

--- src/assets/services/products.js
+++ src/assets/services/products.js
@@ -4,6 +4,10 @@ export function getProducts(limit, skip) {
     return api.get(`/products/?limit=${limit}&skip=${skip}&select=title,price,stock,images`)
 }
 
+export function getProductsByCategory(category) {
+    return api.get(`/products/category/${category}`)
+}
+
 export function getCategories() {
     return api.get(`/products/category-list`)
 }
