35fe51c5d95b8f518a0d2a706e89136d2f9e84db
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-30 07:28:06 -0300

    feat: adding an event when clicking on the category. receives the value/name of the category to search through the category

--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, defineEmits, onMounted } from 'vue'
 import { getCategories } from '../assets/services/products.js'
 
 const categoriesTotal = ref([])
@@ -7,13 +7,16 @@ const categoriesSide = ref([])
 const limit = 14
 const skip = ref(0)
 
+const emit = defineEmits(['selectCategory'])
+
 const fetchCategories = async () => {
   const { data } = await getCategories()
   categoriesTotal.value = data
-  console.log(categoriesTotal.value)
-
   categoriesSide.value = categoriesTotal.value.slice(skip.value, skip.value+limit)
-  console.log(categoriesSide.value)
+}
+
+function select(category) {
+  emit('selectCategory', category)
 }
 
 const nextPage = () => {
@@ -36,11 +39,12 @@ onMounted(async () => {
 <template>
 <div class="fixed grid grid-rows-16 top-[75px] left-[15px] flex flex-col bg-[#322886] w-[300px] h-[850px] rounded-2xl z-50">
   <h3 class="flex mx-auto text-[#ebebeb] font-bold text-3xl mt-4">Categorias</h3>
-  <div
+  <button
       v-for="category in categoriesSide"
-      class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2">
+      class="flex justify-center items-center w-full h-[40px] text-[#ebebeb] hover:bg-[#2d2377] cursor-pointer mt-2"
+      @click="select(category)">
     {{ category }}
-  </div>
+  </button>
   <div class="flex absolute bottom-1 items-center w-full h-[30px] px-6 mb-2 mt-auto">
     <button @click="prevPage" class="text-[#322886] w-[70px] h-full bg-[#ebebeb] border hover:text-[#ebebeb] hover:bg-transparent hover:border-[#ebebeb] cursor-pointer rounded-md">prev</button>
     <button @click="nextPage" class="text-[#322886] w-[70px] h-full bg-[#ebebeb] border hover:text-[#ebebeb] hover:bg-transparent hover:border-[#ebebeb] cursor-pointer rounded-md ml-auto">next</button>
