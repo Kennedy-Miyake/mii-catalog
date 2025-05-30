8978c1a170613f5cada9a0451b218450e73930e3
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-30 07:32:10 -0300

    feat: created a function to search for all products in the selected category

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { getProducts } from '../assets/services/products.js'
+import { getProducts, getProductsByCategory } from '../assets/services/products.js'
 import { isSidebarOpen } from '../assets/services/sidebar.js'
 import ProductCard from '../components/ProductCardComponent.vue'
 import Sidebar from '../components/SidebarComponent.vue'
@@ -8,24 +7,29 @@ import Sidebar from '../components/SidebarComponent.vue'
 const limit = 21
 const skip = ref(0)
 
-const products = ref(null)
+const products = ref([])
+
+const selectedCategory = ref(null)
 
 const fetchProduct = async () => {
   const { data } = await getProducts(limit, skip.value)
   products.value = data.products
 };
 
+
+async function categorySelected(category) {
+  selectedCategory.value = category
+  skip.value = 0
+  const { data } = await getProductsByCategory(category)
+  products.value = data.products
+}
+
 const nextPage = () => {
   skip.value += limit
-  fetchProduct()
 }
 
 const prevPage = () => {
-  if(skip.value == 0) {
-    return
-  }
   skip.value -= limit
-  fetchProduct()
 }
 
 onMounted(async() => {
@@ -37,9 +41,9 @@ onMounted(async() => {
   <section class="relative grid grid-cols-7 grid-rows-3 w-[1575px] h-[850px] ml-auto mt-[75px] mr-[15px] p-6 rounded-2xl shadow-xl/30">
     <Sidebar
       v-if="isSidebarOpen"
+      @selectCategory="categorySelected"
     />
     <ProductCard
-      v-for="p in products"
       :product="p"
     />
     <div class="absolute bottom-1 right-6 flex gap-16 mr-2">
