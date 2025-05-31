d1635a5f73c2253b24455727431807741ce53c18
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-31 04:14:13 -0300

    feat: skeleton of the product details screen

---
+++ src/views/ProductDetailsView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex w-[1600px] h-[700px] mx-auto my-auto bg-gray-500">
+    <div class="flex flex-col bg-red-500 m-[100px] items-center justify-center w-[500px] h-[500px]">
+      <p>Avaliação</p>
+      <img src="" alt="" class="bg-white w-[400px] h-[400px]">
+      <div class="flex bg-blue-400 w-full py-2">
+        <p class="ml-[50px]">R$ 99.99</p>
+        <p class="ml-auto mr-[50px]">Restam 99</p>
+      </div>
+    </div>
+    <div class="flex flex-col bg-blue-400 w-[1000px] my-[100px]">
+      <div class="mt-[100px]">
+        <h1 class="mb-auto bg-white">Nome do Produto</h1>
+        <h6 class="mb-auto bg-black text-white">Categoria</h6>
+        <p class="mt-auto bg-gray-500">Descrição</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
