e5293ac2222684be992eaa91662e6434e6efa196
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-31 06:41:21 -0300

    feat: the product is searched for by the parameter id and added to the product

--- src/views/ProductDetailsView.vue
+++ src/views/ProductDetailsView.vue
@@ -1,22 +1,26 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { getProductById } from '../assets/services/products.js'
 
+const route = useRoute()
+
+const product = ref(null)
+
+onMounted(async () => {
+  const id = route.params.id
+  const { data } = await getProductById(id)
+  product.value = data
+  console.log(product.value)
+})
 </script>
 
 <template>
-  <div class="flex w-[1600px] h-[700px] mx-auto my-auto bg-gray-500">
-    <div class="flex flex-col bg-red-500 m-[100px] items-center justify-center w-[500px] h-[500px]">
-      <p>Avaliação</p>
-      <img src="" alt="" class="bg-white w-[400px] h-[400px]">
       <div class="flex bg-blue-400 w-full py-2">
-        <p class="ml-[50px]">R$ 99.99</p>
-        <p class="ml-auto mr-[50px]">Restam 99</p>
       </div>
     </div>
     <div class="flex flex-col bg-blue-400 w-[1000px] my-[100px]">
       <div class="mt-[100px]">
-        <h1 class="mb-auto bg-white">Nome do Produto</h1>
-        <h6 class="mb-auto bg-black text-white">Categoria</h6>
-        <p class="mt-auto bg-gray-500">Descrição</p>
       </div>
     </div>
   </div>
