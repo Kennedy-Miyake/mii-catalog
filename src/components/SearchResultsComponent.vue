33b7a1ab64d874cf6a3f208d2b49207555bdfcf6
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-05-31 06:44:59 -0300

    mod: redirecting to the product information page when clicking on a product in the search bar

--- src/components/SearchResultsComponent.vue
+++ src/components/SearchResultsComponent.vue
@@ -5,13 +5,15 @@ defineProps({
 </script>
 
 <template>
-  <div class="flex w-[600px] h-[100px] bg-[#ededed] rounded-lg text-black m-6">
-    <img :src="result.images[0]" alt="" class="w-[80px] h-[70px] m-4">
-    <div class="w-full mt-4 space-y-[10px]">
-      <div class="h-[30px] font-semibold">{{ result.title }}</div>
-      <div class="w-[120px] h-[30px] font-medium">R${{ result.price }}</div>
+  <router-link :to="{ name: 'ProductDetails', params: { id: result.id } } ">
+    <div class="flex w-[600px] h-[100px] bg-[#ededed] rounded-lg text-black m-6">
+      <img :src="result.images[0]" alt="" class="w-[80px] h-[70px] m-4">
+      <div class="w-full mt-4 space-y-[10px]">
+        <div class="h-[30px] font-semibold">{{ result.title }}</div>
+        <div class="w-[120px] h-[30px] font-medium">R${{ result.price }}</div>
+      </div>
     </div>
-  </div>
+  </router-link>
 </template>
 
 <style scoped>
